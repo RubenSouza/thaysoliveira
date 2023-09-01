e56fd44fb4c58e4588cade41760225721ca190e4
RubenSouza <rubem.eliel20212@gmail.com>
2023-09-01 14:14:27 -0300

    lib instalation solution

--- src/components/Students.tsx
+++ src/components/Students.tsx
@@ -66,33 +66,6 @@ const Students = () => {
   return (
     <div className="w-[1200px] flex flex-col items-center justify-center">
       <Title title="Alunos" line="w-[50px]" />
-      {/* <div className="w-full flex justify-between py-10">
-        <div className="flex justify-center items-center">
-          <StudentVideo videoId="7grwEwF4DUU" />
-          <div>
-            <h3 className="text-3xl font-semibold text-primary-300">
-              Lara Costa
-            </h3>
-            <p className="text-[11px] font-sans text-neutral-400">
-              Fico assim sem você / Peça felicidade
-            </p>
-            <ul className="">
-              <li
-                className="text-xs font-sans text-neutral-400 
-                list-disc list-inside"
-              >
-                10 Anos
-              </li>
-              <li
-                className="text-xs font-sans text-neutral-400 
-               list-disc list-inside"
-              >
-                5 meses de aula
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div> */}
       <div className="max-w-full flex flex-wrap justify-between py-10 gap-10">
         {studentsList}
       </div>
