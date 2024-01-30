e4bbe124e75f317116d03e45ea4fb4a0e9011c77
RubenSouza <rubem.eliel20212@gmail.com>
2024-01-30 10:12:03 -0300

    main *

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
+      <Route path="/*" element={<Main />} />
       <Route path="/recital" element={<Event />} />
     </Routes>
   );
