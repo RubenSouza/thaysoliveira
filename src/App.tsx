f35f535ef1d8f2eb1cdffa1beb547b4838a0414f
RubenSouza <rubem.eliel20212@gmail.com>
2024-04-27 11:15:17 -0300

    loading test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-// import Event from "./pages/Event";
-import Main from "./pages/Main";
-import { Routes, Route } from "react-router-dom";
+// import Main from "./pages/Main";
+
+import { lazy, Suspense } from "react";
+import loading from "../src/assets/videos/loading3.webp";
+
+const Main = lazy(() => import("./pages/Main"));
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      {/* <Route path="/recital" element={<Event />} /> */}
-    </Routes>
+    <Suspense
+      fallback={
+        <div className="w-full h-screen flex justify-center items-center">
+          <img src={loading} className="w-[200px] h-[200px]" />
+        </div>
+      }
+    >
+      <Main />
+    </Suspense>
   );
 };
 
