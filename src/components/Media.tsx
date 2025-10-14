42cf628feed9456d5db101d4b797f339f1e3ab39
RubenSouza <rubem.eliel2012@gmail.com>
2025-10-13 22:13:35 -0300

    feat: Replace video list with ThaysVideo component for better modularity

--- src/components/Media.tsx
+++ src/components/Media.tsx
@@ -1,32 +1,14 @@
+import ThaysVideo from './ThaysVideo';
+
 export default function Media() {
-  const videos = [
-    {
-      src: 'https://player.vimeo.com/video/1120496193?badge=0&autopause=0&player_id=0&app_id=58479&title=0&byline=0&portrait=0&loop=1',
-      title: 'J. S. Bach MINUETO EM SOL G (BWV Anh. 114)',
-    },
-    {
-      src: 'https://player.vimeo.com/video/1120496278?badge=0&autopause=0&player_id=0&app_id=58479&title=0&byline=0&portrait=0&loop=1',
-      title: 'Mais perto quero estar (Violão - Fingestyle) - Thays Oliveira',
-    },
-  ];
   return (
     <section id='midia' className='content-section'>
       <div className='container'>
         <h2>Mídia</h2>
         <p className='section-subtitle'>Assista a algumas apresentações.</p>
         <div className='midia-grid'>
-          {videos.map(v => (
-            <div className='video-container' key={v.src}>
-              <iframe
-                src={v.src}
-                title={v.title}
-                frameBorder={0}
-                allow='autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share'
-                referrerPolicy='strict-origin-when-cross-origin'
-                allowFullScreen
-              />
-            </div>
-          ))}
+          <ThaysVideo videoId='u-WY05TNY60' />
+          <ThaysVideo videoId='he4gnkr654A' />
         </div>
       </div>
     </section>
