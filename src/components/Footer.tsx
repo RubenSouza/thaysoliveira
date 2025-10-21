a00d1e70b809a00685ae055029ca007560973719
RubenSouza <rubem.eliel2012@gmail.com>
2025-10-20 21:23:48 -0300

    fix: remove developer credit section from Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,23 +15,6 @@ export default function Footer() {
         &copy; 2025 Thays Oliveira - Professora de Música. Todos os direitos
         reservados.
       </p>
-
-      <div className='dev-credit'>
-        <span>Desenvolvido por:</span>
-        <Link
-          href='https://matheusalveswd.vercel.app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image
-            src='/assets/MWBD/logoM.png'
-            alt='Logo do Desenvolvedor'
-            width={40}
-            height={40}
-            className='disable-interaction'
-          />
-        </Link>
-      </div>
     </footer>
   );
 }
